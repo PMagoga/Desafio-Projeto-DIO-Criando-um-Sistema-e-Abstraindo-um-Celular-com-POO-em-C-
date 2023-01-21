c3dd00d513ec3427ee4e3173ae0cc8260cc6b99d
PMagoga <pmagoga@gmail.com>
2023-01-21 11:39:57 -0300

    Desafio projeto DIO POO em CSharp

---
+++ Models/Iphone.cs
@@ -0,0 +1,13 @@
+namespace DesafioPOO.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, string memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "98199558", modelo: "Modelo A", imei: "1123123", memoria: "64");
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "4654887", modelo: "ModeloB", imei: "987456321", memoria: "128");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
